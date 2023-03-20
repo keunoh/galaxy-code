47146b0e3f5c63f817de7651e34f5b413ac4c157
Keunoh <rmsdhcjswo1@gmail.com>
2023-03-21 00:09:47 +0900

    회원 컨트롤러 생성

---
+++ src/main/java/com/kaltz/galaxy/controller/MemberController.java
@@ -0,0 +1,45 @@
+package com.kaltz.galaxy.controller;
+
+import com.kaltz.galaxy.domain.Member;
+import com.kaltz.galaxy.service.MemberService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createMember(Model model) {
+
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/home";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
