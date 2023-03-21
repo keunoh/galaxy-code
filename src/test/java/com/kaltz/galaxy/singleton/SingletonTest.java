24b81661fb0917fe70d4c7bffb2724d7221cf8d5
Keunoh <rmsdhcjswo1@gmail.com>
2023-03-22 00:15:21 +0900

    싱글톤 테스트

---
+++ src/test/java/com/kaltz/galaxy/singleton/SingletonTest.java
@@ -0,0 +1,27 @@
+package com.kaltz.galaxy.singleton;
+
+import com.kaltz.galaxy.config.AppConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SingletonTest {
+
+    @Test
+    void configurationAppConfig() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean);
+    }
+
+    @Test
+    void configurationBeans() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("beanDefinitionName = " + beanDefinitionName);
+        }
+    }
+}
