74a53120a224233c738b190285241a39de81fe4d
Keunoh <rmsdhcjswo1@gmail.com>
2023-03-22 23:40:15 +0900

    @PostConstruct

--- src/main/java/com/kaltz/galaxy/GalaxyApplication.java
+++ src/main/java/com/kaltz/galaxy/GalaxyApplication.java
@@ -1,13 +1,32 @@
 package com.kaltz.galaxy;
 
+import com.kaltz.galaxy.config.AppConfig;
+import com.kaltz.galaxy.domain.Member;
+import com.kaltz.galaxy.repository.MemberRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class GalaxyApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(GalaxyApplication.class, args);
+	}
+
+	@PostConstruct
+	public void save() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+		for (int i = 0; i < 150; i++) {
+			Member member = new Member("keunoh " + i);
+			memberRepository.save(member);
+		}
 	}
 
 }
